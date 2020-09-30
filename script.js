7fafdea85670193b8baca88e97fbfc2cd8b9ca56
AntoineGGG <a.gorenflot@gmx.fr>
2020-09-30 16:34:34 +0200

    Ajout de JS pour menu burger

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const title = document.querySelector('.title');
 burger.onclick = function () {
   burgerLogo.style.display = 'none';
   title.style.display = 'none';
+  navBar.style.width = '100%';
   navBar.style.display = 'flex';
+  navBar.style.flexDirection = 'row';
+
+  navBar.style.alignItems = 'center';
+  navBar.style.justifyContent = 'space-between';
 };
