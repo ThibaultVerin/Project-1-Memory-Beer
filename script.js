8205615d5bb67626c0c1b2c9d7e7c0ac67ddfa00
Jean Ducret <jean.dcrt@gmail.com>
2020-10-01 09:26:35 +0200

    Revert "Merge pull request #5 from ThibaultVerin/dev"
    This reverts commit 465d37281830b4757db093e302722470cfc9db08, reversing
    changes made to ee37227b201e5c175cf94d581afb32bf00e588e6.

--- script.js
+++
@@ -1,8 +0,0 @@
-const burgerLogo = document.querySelector('#burger-logo');
-const navBar = document.querySelector('#navbar');
-const title = document.querySelector('.title');
-burger.onclick = function () {
-  burgerLogo.style.display = 'none';
-  title.style.display = 'none';
-  navBar.style.display = 'flex';
-};
