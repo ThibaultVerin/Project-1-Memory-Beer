16acc49473c2dbe9b1662e859d6634c0e5ba2d45
Thibault Verin <thibaultverin@hotmail.com>
2020-10-08 15:35:07 +0200

    Test pour le tuto

--- tutorial-script.js
+++ tutorial-script.js
@@ -35,50 +35,61 @@ function startTutorial() {
   }, 1500);
 }
 
-/*
-Prochaines étapes: 
-    - Modification du texte
-    - Modification des classes sur les cartes
-*/
+const card = document.querySelectorAll('.card');
+const arrayCard = [...card];
+let clicked = false;
+let firstCard;
+let secondCard;
+let lockBoard = false;
 
-function step1Tutorial() {
-  tutorialBtn.innerHTML = 'Click on a second card';
-  firstCard[0].classList.replace('card-clignote', 'card');
-  firstCard[5].classList.replace('card', 'card-clignote');
+const displayCard = function () {
+  if (this === firstCard) {
+    return;
+  }
+
+  if (lockBoard) {
+    return;
+  }
+
+  this.classList.toggle('open');
+
+  if (!clicked) {
+    clicked = true;
+    firstCard = this;
+    console.log('firstCard');
+  } else {
+    clicked = false;
+    secondCard = this;
+    console.log('secondCard');
+
+    /*Code pour gérer l'event DRUNK MODE */
+
+    if (drunk) {
+      myContainer.addEventListener('click', () => {
+        myContainer.classList.add('rotate');
+      });
+    }
+
+    match();
+  }
+};
+
+function match() {
+  // SI SUCCES
+  if (firstCard.dataset.index === secondCard.dataset.index) {
+    firstCard.removeEventListener('click', displayCard);
+    secondCard.removeEventListener('click', displayCard);
+    console.log('its a match');
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove('open');
+      secondCard.classList.remove('open');
+      lockBoard = false;
+      firstCard = null;
+    }, 1000);
+  }
 }
 
-function step2Tutorial() {
-  firstCard[0].classList.replace('card-clignote', 'card');
-  firstCard[5].classList.replace('card-clignote', 'card');
-
-  tutorialBtn.innerHTML = 'Try to find the match !';
-  setTimeout(() => {
-    firstCard[4].classList.replace('card', 'card-clignote');
-  }, 1000);
-}
-
-function step3Tutorial() {
-  firstCard[4].classList.replace('card-clignote', 'card');
-  firstCard[12].classList.replace('card', 'card-clignote');
-}
-
-/*
-Dernière étape: 
-    - Incrémentation du score actuel (ici à 3 pour mieux voir la jauge progresser)
-    - Création d'une variable currentPercent qui définit la taille de la jauge en % en fonction du score
-    - Modificiation de la taille de la jauge en fonction de la variable currentPercent + transition
-    - Retour du background à la couleur initiale et modification de bouton après 4s
-*/
-
-function step4Tutorial() {
-  tutorialBtn.innerHTML = 'If you match, you are getting drunker !';
-  currentScore += 3;
-  let currentPercent = (currentScore / totalPaires) * 100;
-  jaugeContainer.style.height = `${currentPercent}%`;
-  jaugeContainer.style.transition = `height 2s ease-out`;
-  setTimeout(() => {
-    mainBackground.style.background = '#f26d3d';
-    mainBackground.style.transition = 'all 3s ease';
-    tutorialBtn.innerHTML = "<a href='game.html'>Let's play ! </a>";
-  }, 4000);
+for (let i = 0; i < arrayCard.length; i++) {
+  arrayCard[i].addEventListener('click', displayCard);
 }
